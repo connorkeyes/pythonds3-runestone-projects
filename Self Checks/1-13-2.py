21c82e691db667271d21ba6396b46c9a4953286c
Connor Keyes <76636011+connorkeyes@users.noreply.github.com>
2022-07-31 16:55:56 -0700

    Update 1-13-2.py

---
+++ Self Checks/1-13-2.py
@@ -0,0 +1,170 @@
+class LogicGate:
+
+    def __init__(self, lbl):
+        self.name = lbl
+        self.output = None
+
+    def get_label(self):
+        return self.name
+
+    def get_output(self):
+        self.output = self.perform_gate_logic()
+        return self.output
+
+
+class BinaryGate(LogicGate):
+
+    def __init__(self, lbl):
+        super(BinaryGate, self).__init__(lbl)
+
+        self.pin_a = None
+        self.pin_b = None
+
+    def get_pin_a(self):
+        if self.pin_a == None:
+            return int(input("Enter pin A input for gate " + self.get_label() + ": "))
+        else:
+            return self.pin_a.get_from().get_output()
+
+    def get_pin_b(self):
+        if self.pin_b == None:
+            return int(input("Enter pin B input for gate " + self.get_label() + ": "))
+        else:
+            return self.pin_b.get_from().get_output()
+
+    def set_next_pin(self, source):
+        if self.pin_a == None:
+            self.pin_a = source
+        else:
+            if self.pin_b == None:
+                self.pin_b = source
+            else:
+                print("Cannot Connect: NO EMPTY PINS on this gate")
+
+
+class AndGate(BinaryGate):
+
+    def __init__(self, lbl):
+        BinaryGate.__init__(self, lbl)
+
+    def perform_gate_logic(self):
+
+        a = self.get_pin_a()
+        b = self.get_pin_b()
+        if a == 1 and b == 1:
+            return 1
+        else:
+            return 0
+
+
+class NandGate(BinaryGate):
+
+    def __init__(self, lbl):
+        BinaryGate.__init__(self, lbl)
+
+    def perform_gate_logic(self):
+
+        a = self.get_pin_a()
+        b = self.get_pin_b()
+        if a == 1 and b == 1:
+            return 0
+        else:
+            return 1
+
+class OrGate(BinaryGate):
+
+    def __init__(self, lbl):
+        BinaryGate.__init__(self, lbl)
+
+    def perform_gate_logic(self):
+
+        a = self.get_pin_a()
+        b = self.get_pin_b()
+        if a == 1 or b == 1:
+            return 1
+        else:
+            return 0
+
+
+class NorGate(BinaryGate):
+
+    def __init__(self, lbl):
+        BinaryGate.__init__(self, lbl)
+
+    def perform_gate_logic(self):
+
+        a = self.get_pin_a()
+        b = self.get_pin_b()
+        if a == 1 or b == 1:
+            return 0
+        else:
+            return 1
+
+class UnaryGate(LogicGate):
+
+    def __init__(self, lbl):
+        LogicGate.__init__(self, lbl)
+
+        self.pin = None
+
+    def get_pin(self):
+        if self.pin == None:
+            return int(input("Enter pin input for gate " + self.get_label() + ": "))
+        else:
+            return self.pin.get_from().get_output()
+
+    def set_next_pin(self, source):
+        if self.pin == None:
+            self.pin = source
+        else:
+            print("Cannot Connect: NO EMPTY PINS on this gate")
+
+
+class NotGate(UnaryGate):
+
+    def __init__(self, lbl):
+        UnaryGate.__init__(self, lbl)
+
+    def perform_gate_logic(self):
+        if self.get_pin():
+            return 0
+        else:
+            return 1
+
+
+class Connector:
+
+    def __init__(self, fgate, tgate):
+        self.from_gate = fgate
+        self.to_gate = tgate
+
+        tgate.set_next_pin(self)
+
+    def get_from(self):
+        return self.from_gate
+
+    def get_to(self):
+        return self.to_gate
+
+
+def main(): #prove !((A and B) or (C and D)) == !(A and B) and !(C and D)
+    g1 = AndGate("G1") # A and B
+    g2 = AndGate("G2") # C and D
+    g3 = NorGate("G3") # G1 nor G2
+    g4 = NandGate("G4") # !(A and B)
+    g5 = NandGate("G5") # !(B and C)
+    g6 = AndGate("G6") # G4 and G5
+    c1 = Connector(g1, g3)
+    c2 = Connector(g2, g3)
+    c3 = Connector(g4, g6)
+    c4 = Connector(g5, g6)
+    
+    stmt_1 = g3.get_output()
+    stmt_2 = g6.get_output()
+    
+    if stmt_1 == stmt_2:
+        print("Tautology.")
+    else:
+        print("No tautology. Ensure inputs are correct.")
+
+main()
